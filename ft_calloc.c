b9ad1fd6556e5523a92ea86d0e02a24a73c42923
Hoomi1 <glebbambam@yandex.ru>
2020-11-08 20:03:12 +0300

    home

---
+++ ft_calloc.c
@@ -0,0 +1,15 @@
+
+#include "libft.h"
+
+void *ft_calloc(size_t number, size_t size)
+{
+	char	*s;
+	size_t	sum;
+
+	sum = number * size;
+	s = (char *) malloc(sum);
+	if (!s)
+		return (NULL);
+	ft_bzero(s, sum);
+	return (s);
+}
