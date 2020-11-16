4ce3ef2eba33b72a9b89e543801d126d86442f35
Hoomi1 <glebbambam@yandex.ru>
2020-11-16 15:58:26 +0300

    home_putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -12,46 +12,21 @@
 
 #include "libft.h"
 
-int		power(int n)
+void	ft_putnbr_fd(int n, int fd)
 {
-	size_t p = 1;
-	int i = 1;
-	ft_itoa(n);
-	while (n[i])
-	{
-		p = p * 10;
-		i++ ;
-	}
-	return (p);
-}
-
-void		ft_putnbr_fd(int n, int fd)
-{
-	size_t i;
-	int a;
-	int k;
+	long	i;
 
 	i = n;
-	k = 0;
-	a = 1;
 	if (i < 0)
 	{
 		ft_putchar_fd('-', fd);
 		i = -i;
 	}
-	power(i);
-	while (i[k])
+	if (i >= 10)
 	{
-		ft_putchar_fd(*i, fd);
-		i++;
+		ft_putnbr_fd(i / 10, fd);
+		ft_putchar_fd(i % 10 + '0', fd);
 	}
-	/*while ((i = i/10) > 0)
-		a *= 10;
-	while (a > 0)
-	{
-		i = n % 1;
-		ft_putchar_fd(i + '0', fd);
-		n -= i * 10;
-		a /= 10;
-	}*/
+	if (i < 10)
+		ft_putchar_fd(i % 10 + '0', fd);
 }
