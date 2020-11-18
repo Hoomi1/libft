695be891e618063ac52aba13f739e069f8b0ffe1
Cresswell Yuuki <cyuuki@at-g5.msk.21-school.ru>
2020-11-18 23:23:17 +0300

    school 1.3

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,56 +6,28 @@
 /*   By: cyuuki <cyuuki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 16:57:47 by cyuuki            #+#    #+#             */
-/*   Updated: 2020/11/18 17:34:00 by cyuuki           ###   ########.fr       */
+/*   Updated: 2020/11/18 23:19:59 by cyuuki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	int		ft_first(char const *s1, char const *set)
+char		*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	i;
-	size_t	len;
-
-	i = 0;
-	len = ft_strlen(s1);
-	while (i < len)
-	{
-		if (ft_strchr(set, s1[i]) == 0)
-			break ;
-		i++;
-	}
-	return (i);
-}
-
-static	int		ft_last(char const *s1, char const *set)
-{
-	size_t	i;
-	size_t	len;
-
-	i = 0;
-	len = ft_strlen(s1);
-	while (i < len)
-	{
-		if (ft_strchr(set, s1[len]) == 0)
-			break ;
-		len--;
-	}
-	return (len);
-}
-
-char			*ft_strtrim(char const *s1, char const *set)
-{
-	int		l;
-	int		f;
+	size_t	l;
+	size_t	f;
 	char	*str2;
 
-	if (s1 == NULL)
+	l = 0;
+	if (!s1 || !set)
 		return (NULL);
-	f = ft_first(s1, set);
-	l = ft_last(s1, set);
-	if (f >= l)
+	f = ft_strlen(s1);
+	while (ft_strchr(set, s1[l]))
+		l++;
+	if (l == f)
 		return (ft_strdup(""));
-	str2 = ft_substr(s1, f, l - f + 1);
+	while (ft_strchr(set, s1[f]))
+		f--;
+	str2 = ft_substr(s1, l, f - l + 1);
 	return (str2);
 }
